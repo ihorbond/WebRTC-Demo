ad795745f5af952c7ba58f8ea5e6a0a2c28ef128
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-05-08 13:44:14 -0400

    add video element and stream video from web cam

--- WebRTCDemo/ClientApp/src/app/components/home/home.component.ts
+++ WebRTCDemo/ClientApp/src/app/components/home/home.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+
+const mediaStreamConstraints: MediaStreamConstraints = {
+  video: true
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('video') videoEl: ElementRef;
+
+  private localStream: MediaStream;
+  private videoPlayer: HTMLVideoElement;
+
+  constructor(private _renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.videoPlayer = this._renderer.selectRootElement(this.videoEl.nativeElement);
+
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+      .then((stream: MediaStream) => {
+        this.localStream = stream;
+        this.videoPlayer.srcObject = stream;
+      })
+      .catch((err: string) => {
+        alert(err);
+      });
+  }
+
 }
